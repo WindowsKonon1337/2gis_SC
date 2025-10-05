2c9b2e44e7f6301067da47322344d272c3197873
Maksim Kononykhin <kononelder@gmail.com>
2025-10-05 11:54:55 +0300

    updated code

--- src/services/llm_service/prompts.py
+++ src/services/llm_service/prompts.py
@@ -10,7 +10,7 @@ You need to estimate the following parameters from the image:
 As a response, generate a JSON with the following fields and nothing else:
 - load: (string) Estimated bus occupancy. One of three possible states: free, average, or full
 - people_num: (int) Actual number of people
-- free_entrance: (int or array) Freest entrance(s). Analyze the distribution of people inside the bus and determine the freest entrance based on the provided gate order. If gate_num is a single number, return that number or 0 if all exits are full. If gate_num is an array, return an array of freest entrances in order of preference or [0] if all exits are full.
+- free_entrance: (int or array) Freest entrance(s). Analyze the distribution of people inside the bus and determine the freest entrance based on the provided gate order. If gate_num is a single number, return that number or 0 if all exits are full. If gate_num is an array, return an array of freest entrances in order of preference or [0] if all exits are full. If load is full - always wrtie [0] despite of REALLY exclusive cases.
 - free_seats: (int) Actual number of free seats in bus
 
 Input parameters:
